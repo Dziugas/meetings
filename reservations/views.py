cccf8eb8a158dd5588bb5ef93be968be3b12ab5e
Dziugas <dziugas.tornau@gmail.com>
2021-02-10 19:39:55 +0200

    add filtering by employee to reservations endpoint

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query_utils import Q
 from rest_framework import viewsets
 from .models import MeetingRoom, Reservation, Invitation
 from .serializers import (
@@ -8,8 +9,20 @@ from .serializers import (
 
 
 class ReservationViewset(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        """
+        Shows only reservations related to the provided user by the
+        provided user_id query parameter in the URL
+        """
+        queryset = Reservation.objects.all()
+        user_id = self.request.query_params.get("user_id", None)
+        if user_id is not None:
+            queryset = queryset.filter(
+                Q(creator__id=user_id) | Q(attendees__id=user_id)
+            )
+        return queryset
 
 
 class InvitationViewset(viewsets.ModelViewSet):
