dd5dd21d708f1e30aacd8cdb58237171a2104600
Dziugas <dziugas.tornau@gmail.com>
2021-02-11 12:06:56 +0200

    add djangorestframework Token authentication

--- meetings/settings.py
+++ meetings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'reservations',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- meetings/urls.py
+++ meetings/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from accounts.urls import router as accounts_router
 from reservations.urls import router as reservations_router
+
 
 router = routers.DefaultRouter()
 router.registry.extend(accounts_router.registry)
@@ -11,4 +13,5 @@ router.registry.extend(reservations_router.registry)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api-token-auth/", obtain_auth_token),
 ]
